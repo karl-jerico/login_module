81a1e24d0ff8a53d51254176862c4d9baf65a045
karl-jerico <jerico.jeks@gmail.com>
2024-04-04 17:59:37 +0800

    Create crudd

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,10 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="flex flex-col items-center justify-center bg-white overflow-hidden shadow-sm sm:rounded-lg p-4">
                 <div class="p-6 text-gray-900">
                     {{ __("You're logged in!") }}
                 </div>
+                <a href="{{ route('roles.index') }}"
+                class=" float-end text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  focus:outline-none ">User Management</a>
             </div>
         </div>
     </div>
---
+++ resources/views/roles/index.blade.php
@@ -0,0 +1,61 @@
+<x-app-layout>
+
+    <div class="flex flex-col items-center my-8">
+        <div class="card">
+            <div class="flex items-center justify-between p-4 bg-white border-b-2 rounded-xl w-full ">
+                <h4>User Management</h4>
+                <a href="{{ route('roles.create') }}"
+                    class=" float-end text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5  focus:outline-none ">Add
+                    User</a>
+            </div>
+            <div class="card-body">
+                <div class="relative overflow-x-auto w-full shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" class="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Email
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Role
+                                </th>
+                                <th scope="col" class="px-6 py-3">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr class="bg-white dark:bg-gray-800">
+                                <th scope="row"
+                                    class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                    {{ $user->name }}
+                                </th>
+                                <td class="px-6 py-4">
+                                    {{ $user->email }}
+                                </td>
+                                <td class="px-6 py-4">
+                                    <!-- Assuming you want to display 'Accessories' for all users -->
+                                    Testt
+                                </td>
+                                <td class="flex gap-4 px-6 py-4">
+                                    <a href="{{ route('roles.edit', $user->id) }}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                    <form action="{{ route('roles.destroy', $user->id) }}" method="POST" onsubmit="return confirm('Are you sure you want to delete this user?')">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="font-medium text-red-600 dark:text-blue-500 hover:underline">Delete</button>
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
